11e816dc22ee47cf4d9ae044154174b53ff6b781
KelvinIO <kelviinvn@protonmail.com>
2021-09-15 13:00:16 +1000

    Implemented a-star algorithm and finished keybinds.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pathfinder")
 
 # Define colors for grid coloring using RGB format
+BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0) # Start position
 PURPLE = (255, 0, 255) # End position
 RED = (255, 0, 0) # Already visited
@@ -71,9 +72,9 @@ class Node:
         self.color = PURPLE
 
     def makePath(self):
-        self.color = WHITE  # change to more visible color later
+        self.color = BLUE  
 
-    # Define the methods to draw the nodes on screen
+# Define the methods to draw the nodes on screen
     def draw(self, window):
         pygame.draw.rect(window, self.color, (self.x, self.y, self.width, self.height))
 
@@ -101,6 +102,63 @@ def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+def remakePath(prevNode, current, draw):
+    while current in prevNode:
+        current = prevNode[current]
+        current.makePath()
+        draw()
+
+# A-Star Algorithm 
+def astar(draw, grid, start, end):
+    count = 0
+    openSet = PriorityQueue()
+    openSet.put((0, count, start)) # Initialize the open set with our F(n), a count for tiebreaker purposes and start node
+    prevNode = {}
+
+    # Intilize g(n) and f(n) as infinity for all nodes except start to calculate path costs
+    gScore = {node: float("inf") for row in grid for node in row} 
+    gScore[start] = 0
+    fScore = {node: float("inf") for row in grid for node in row} 
+    fScore[start] = h(start.getPosition(), end.getPosition())
+    
+    # Keep track of items in priority queue using a hash
+    openSetHash = {start}
+
+    while not openSet.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.key == pygame.K_q:
+                pygame.quit()
+
+        current = openSet.get()[2] # Current node we're evaluating
+        openSetHash.remove(current)
+
+        if current == end:
+            remakePath(prevNode, end, draw)
+            # Reset the colors of start and end nodes
+            start.makeStart()
+            end.makeEnd()
+            return True # we found the path!
+        
+        # Evaluate f/g scores of the current and neighbor nodes
+        for neighbor in current.neighbors:
+            tempGscore = gScore[current] + 1
+
+            if tempGscore < gScore[neighbor]: # Update pathing if a better path is found
+                prevNode[neighbor] = current
+                gScore[neighbor] = tempGscore
+                fScore[neighbor] = tempGscore + h(neighbor.getPosition(), end.getPosition())
+
+                if neighbor not in openSetHash:
+                    count += 1
+                    openSet.put((fScore[neighbor], count, neighbor))
+                    openSetHash.add(neighbor)
+                    neighbor.makeOpen()
+        draw()
+
+        if current != start: # If the current node is not the start node...
+            current.makeClosed() # Make it closed as we've already considered it's f/g/h scores in the prev for loop
+    return False
 
 # Make the grid
 def makeGrid(rows, width):
@@ -192,9 +250,14 @@ def main(window, width):
                 if event.key == pygame.K_SPACE and not started:
                     for row in grid:
                         for node in row:
-                            node.updateNeighbors()
+                            node.updateNeighbors(grid)
 
                     astar(lambda: drawGrid(window, grid, ROWS, width), grid, start, end)
+
+                if event.key == pygame.K_c: # Reset the grid
+                    start = None
+                    end = None
+                    grid = makeGrid(ROWS, width)
 
     pygame.quit()
 
